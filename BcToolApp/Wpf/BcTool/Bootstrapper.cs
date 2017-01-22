a9a7923dfa1c0125f83c34671f8aed83233cd0f9
畑中 拓 <BOG-Taku_Hatanaka@jmas.co.jp>
2017-01-22 23:16:50 +0900

    WPFにPrismを適用

---
+++ BcToolApp/Wpf/BcTool/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Practices.Unity;
+using Prism.Unity;
+using BcTool.Views;
+using System.Windows;
+
+namespace BcTool
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
