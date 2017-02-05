b15f778130544703c14430bda38f9f5dc97cd597
畑中 拓 <BOG-Taku_Hatanaka@jmas.co.jp>
2017-02-05 12:07:10 +0900

    改行コードを修正

--- BcToolApp/Xamarin/BcTool/BcTool/App.xaml.cs
+++ BcToolApp/Xamarin/BcTool/BcTool/App.xaml.cs
@@ -4,29 +4,29 @@ using Xamarin.Forms;
 
 namespace BcTool
 {
-    public partial class App : PrismApplication
-    {
-        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+	public partial class App : PrismApplication
+	{
+		public App(IPlatformInitializer initializer = null) : base(initializer) { }
 
-        protected override void OnInitialized()
-        {
-            InitializeComponent();
+		protected override void OnInitialized()
+		{
+			InitializeComponent();
 
-            NavigationService.NavigateAsync(nameof(MasterPage));
-            //NavigationService.NavigateAsync("NavigationPage/" + nameof(BulletinBoardPage));
-        }
+			NavigationService.NavigateAsync(nameof(MasterPage));
+			//NavigationService.NavigateAsync("NavigationPage/" + nameof(BulletinBoardPage));
+		}
 
-        protected override void RegisterTypes()
-        {
-            Container.RegisterTypeForNavigation<NavigationPage>();
-            Container.RegisterTypeForNavigation<MainPage>();
-            Container.RegisterTypeForNavigation<SigninPage>();
-            Container.RegisterTypeForNavigation<BulletinBoardListPage>();
+		protected override void RegisterTypes()
+		{
+			Container.RegisterTypeForNavigation<NavigationPage>();
+			Container.RegisterTypeForNavigation<MainPage>();
+			Container.RegisterTypeForNavigation<SigninPage>();
+			Container.RegisterTypeForNavigation<BulletinBoardListPage>();
 			Container.RegisterTypeForNavigation<MenuPage>();
 			Container.RegisterTypeForNavigation<MasterPage>();
 			Container.RegisterTypeForNavigation<SettingPage>();
-            Container.RegisterTypeForNavigation<BulletinBoardPage>();
-            Container.RegisterTypeForNavigation<UserInfoListPage>();
-        }
-    }
+			Container.RegisterTypeForNavigation<BulletinBoardPage>();
+			Container.RegisterTypeForNavigation<UserInfoListPage>();
+		}
+	}
 }
