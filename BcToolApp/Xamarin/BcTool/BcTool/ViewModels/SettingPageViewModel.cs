56ef0f11bf9774054a034a5183b6fc8b046b8677
高橋 辰伍 <takahashi@be-org.co.jp>
2017-03-05 23:04:44 +0900

    SettingPageの処理をViewModelへ移動

--- BcToolApp/Xamarin/BcTool/BcTool/ViewModels/SettingPageViewModel.cs
+++ BcToolApp/Xamarin/BcTool/BcTool/ViewModels/SettingPageViewModel.cs
@@ -1,16 +1,141 @@
-﻿using Prism.Commands;
+﻿using BcTool.Views;
 using Prism.Mvvm;
-using System;
+using Prism.Navigation;
+using Prism.Services;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace BcTool.ViewModels
 {
+	/// <summary>
+	/// 設定画面ViewModel
+	/// </summary>
 	public class SettingPageViewModel : BindableBase
 	{
-		public SettingPageViewModel()
-		{
+		#region メンバー変数
 
+		/// <summary>
+		/// ナビゲーションサービス
+		/// </summary>
+		private readonly INavigationService _navigationService;
+
+		/// <summary>
+		/// ダイアログサービス
+		/// </summary>
+		private readonly IPageDialogService _pageDialogService;
+
+		#endregion
+
+		#region コンストラクタ
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		/// <param name="navigationService">ナビゲーションサービス</param>
+		/// <param name="pageDialogService">ダイアログサービス</param>
+		public SettingPageViewModel(INavigationService navigationService, IPageDialogService pageDialogService)
+		{
+			_navigationService = navigationService;
+			_pageDialogService = pageDialogService;
+
+			// 更新履歴のHtmlファイルを読み込む
+			Assembly assembly = typeof(SettingPage).GetTypeInfo().Assembly;
+			using (Stream stream = assembly.GetManifestResourceStream("BcTool.Resources.Html.ChangeLog.html"))
+			{
+				using (StreamReader reader = new StreamReader(stream))
+				{
+					string str = reader.ReadToEnd();
+					ChangeLogSource = new HtmlWebViewSource
+					{
+						Html = str
+					};
+				}
+			}
 		}
+		#endregion
+
+		#region プロパティ
+
+		/// <summary>
+		/// 掲示板同期期間リスト
+		/// </summary>
+		public List<string> SyncPeriodItems
+		{
+			get
+			{
+				return new List<string>
+				{
+					"７日間",
+					"１４日間",
+					"１ヶ月間",
+					"３ヶ月間",
+					"６ヶ月間",
+					"１年間",
+					"すべて",
+				};
+			}
+		}
+
+		private HtmlWebViewSource _changeLogSource;
+		/// <summary>
+		/// 更新履歴WebViewSource
+		/// </summary>
+		public HtmlWebViewSource ChangeLogSource
+		{
+			get
+			{
+				return _changeLogSource;
+			}
+			set
+			{
+				SetProperty(ref _changeLogSource, value);
+			}
+		}
+
+		#endregion
+
+		#region コマンド
+
+		private ICommand _changePasswordCommand = null;
+		/// <summary>
+		/// パスワード変更タップコマンド
+		/// </summary>
+		public ICommand ChangePasswordCommand => _changePasswordCommand ?? (
+			_changePasswordCommand = new Command(async () => await executeNavigatePasswordChange()));
+
+		private ICommand _signoutCommand = null;
+		/// <summary>
+		/// サインアウトタップコマンド
+		/// </summary>
+		public ICommand SignoutCommand => _signoutCommand ?? (
+			_signoutCommand = new Command(async () => await executeSignout()));
+
+		#endregion
+
+		#region メソッド
+
+		/// <summary>
+		/// パスワード変更画面へ遷移する
+		/// </summary>
+		private async Task executeNavigatePasswordChange()
+		{
+			// パスワード変更画面に遷移する
+			await _navigationService.NavigateAsync("/MasterPage/NavigationPage/PasswordChangePage");
+		}
+
+		/// <summary>
+		/// サインアウトする
+		/// </summary>
+		/// <returns></returns>
+		private async Task executeSignout()
+		{
+			// サインイン画面へ遷移する
+			await _navigationService.NavigateAsync("/SigninPage");
+		}
+
+		#endregion
 	}
 }
--- BcToolApp/Xamarin/BcTool/BcTool/Views/SettingPage.xaml.cs
+++ BcToolApp/Xamarin/BcTool/BcTool/Views/SettingPage.xaml.cs
@@ -11,34 +11,5 @@ namespace BcTool.Views
 		{
 			InitializeComponent();
 		}
-
-		protected async override void OnAppearing()
-		{
-			base.OnAppearing();
-
-			pcrSyncPeriod.ItemsSource = new List<string>()
-			{
-				"７日間",
-				"１４日間",
-				"１ヶ月間",
-				"３ヶ月間",
-				"６ヶ月間",
-				"１年間",
-				"すべて"
-			};
-
-			Assembly assembly = typeof(SettingPage).GetTypeInfo().Assembly;
-			using (Stream stream = assembly.GetManifestResourceStream("BcTool.Resources.Html.ChangeLog.html"))
-			{
-				using (StreamReader reader = new StreamReader(stream))
-				{
-					string str = await reader.ReadToEndAsync();
-					wvChangeLog.Source = new HtmlWebViewSource
-					{
-						Html = str
-					};
-				}
-			}
-		}
 	}
 }
