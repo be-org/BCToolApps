66c4abfef6608231bd0cdaf626c255f8df8a3861
高橋 辰伍 <takahashi@be-org.co.jp>
2017-03-05 16:55:07 +0900

    SyncPeriodDataModel追加

---
+++ BcToolApp/Xamarin/BcTool/BcTool/DataModels/SyncPeriodDataModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BcTool.DataModels
+{
+	/// <summary>
+	/// 掲示板同期期間用データモデル
+	/// </summary>
+	public class SyncPeriodDataModel
+	{
+		private string _syncPeriod;
+		/// <summary>
+		/// 掲示板同期期間
+		/// </summary>
+		public string SyncPeriod
+		{
+			get
+			{
+				return _syncPeriod;
+			}
+			set
+			{
+				_syncPeriod = value;
+			}
+		}
+	}
+}
