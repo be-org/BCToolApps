5ee5eb26ff9947f2a78cd841a0a3efb70241c33c
Taku Hatanaka <taku-hatanaka@be-org.co.jp>
2017-03-05 12:44:14 +0900

    ListViewの選択イベントのビヘイビアクラスを修正

--- BcToolApp/Xamarin/BcTool/BcTool/Behaviors/NotSelectableListViewBehavior.cs
+++ BcToolApp/Xamarin/BcTool/BcTool/Behaviors/NotSelectableListViewBehavior.cs
@@ -1,30 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace BcTool.Behaviors
 {
+    /// <summary>
+    /// ListViewの選択した行を色を付けないビヘイビアクラス
+    /// </summary>
     public class NotSelectableListViewBehavior : BindableBehavior<ListView>
     {
+        #region コンストラクタ
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public NotSelectableListViewBehavior()
+        {
+
+        }
+
+        #endregion
+
+        #region 公開メソッド
+
+        /// <summary>
+        /// アタッチ
+        /// </summary>
+        /// <param name="bindable">ListViewのインスタンス</param>
         protected override void OnAttachedTo(ListView bindable)
         {
             base.OnAttachedTo(bindable);
-
-            bindable.ItemSelected += this.Bindable_ItemSelected;
+            bindable.ItemSelected += OnItemSelected;
         }
 
+        /// <summary>
+        /// デタッチ
+        /// </summary>
+        /// <param name="bindable">ListView</param>
         protected override void OnDetachingFrom(ListView bindable)
         {
             base.OnDetachingFrom(bindable);
-            bindable.ItemSelected -= this.Bindable_ItemSelected;
+            bindable.ItemSelected -= OnItemSelected;
         }
 
-        private void Bindable_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        #endregion
+
+        #region イベントメソッド
+
+        /// <summary>
+        /// アイテム選択イベント処理
+        /// </summary>
+        /// <param name="sender">イベント元情報</param>
+        /// <param name="e">イベント引数</param>
+        private void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
             ((ListView)sender).SelectedItem = null;
         }
+
+        #endregion
     }
 }
---
+++ BcToolApp/Xamarin/BcTool/BcTool/Converters/SelectedItemConverter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace BcTool.Converters
+{
+    /// <summary>
+    /// 選択アイテムのコンバータークラス
+    /// </summary>
+    public class SelectedItemConverter : IValueConverter
+    {
+        #region コンストラクタ
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public SelectedItemConverter()
+        {
+
+        }
+
+        #endregion
+
+        #region 公開メソッド
+
+        /// <summary>
+        /// コンバート
+        /// </summary>
+        /// <param name="value">値</param>
+        /// <param name="targetType">型</param>
+        /// <param name="parameter">パラメータ</param>
+        /// <param name="culture">カルチャー</param>
+        /// <returns>コンバートしたオブジェクト</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var args = (SelectedItemChangedEventArgs)value;
+            return args.SelectedItem;
+        }
+
+        /// <summary>
+        /// コンバートバック
+        /// </summary>
+        /// <param name="value">値</param>
+        /// <param name="targetType">型</param>
+        /// <param name="parameter">パラメータ</param>
+        /// <param name="culture">カルチャー</param>
+        /// <returns>コンバートを戻したオブジェクト</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
