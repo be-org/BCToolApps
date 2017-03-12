2b1fe91a46e55eee438aff792268886f5415f354
onoda <onoda>
2017-03-12 18:13:39 +0900

    ユーザーデータモデルを追加

---
+++ BcToolApp/Xamarin/BcTool/BcTool/DataModels/UserDataModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BcTool.DataModels
+{
+    public class UserDataModel
+    {
+        #region
+        /// <summary>
+        /// ユーザー名
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// メールアドレス
+        /// </summary>
+        public string UserId { get; set; }
+        /// <summary>
+        /// パスワード
+        /// </summary>
+        public string Password { get; set; }
+        /// <summary>
+        /// 所属グループ
+        /// </summary>
+        public string Group { get; set; }
+
+        #endregion
+    }
+}
