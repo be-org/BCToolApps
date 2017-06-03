1dd5a6b2745dbf4340149e6445567bf63c153684
T.Hatanaka <taku-hatanaka@be-org.co.jp>
2017-06-03 20:10:34 +0900

    iOSのListViewのLineの長さを調整するようにレンダラーを実装

---
+++ BcToolApp/Xamarin/BcTool/BcTool.iOS/Renderers/CustomListViewRenderer.cs
@@ -0,0 +1,42 @@
+﻿using BcTool.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ListView), typeof(CustomListViewRenderer))]
+namespace BcTool.iOS.Renderers
+{
+    /// <summary>
+    /// Custom list view renderer.
+    /// </summary>
+    public class CustomListViewRenderer : ListViewRenderer
+    {
+        /// <summary>
+        /// Ons the element changed.
+        /// </summary>
+        /// <param name="e">E.</param>
+		protected override void OnElementChanged(ElementChangedEventArgs<ListView> e)
+		{
+			base.OnElementChanged(e);
+
+            if (Control == null)
+            {
+                return;
+            }
+
+			// ListViewのセパレータの余白をゼロにします。
+			Control.SeparatorInset = UIEdgeInsets.Zero;
+			if (UIDevice.CurrentDevice.CheckSystemVersion(8, 0))
+			{
+				Control.LayoutMargins = UIEdgeInsets.Zero;
+			}
+			if (UIDevice.CurrentDevice.CheckSystemVersion(9, 0))
+			{
+				Control.CellLayoutMarginsFollowReadableWidth = false;
+			}
+
+			// ListView Separator - データが無い部分の線を消します。
+			Control.TableFooterView = new UIView();
+		}
+    }
+}
---
+++ BcToolApp/Xamarin/BcTool/BcTool.iOS/Renderers/CustomViewCellRenderer.cs
@@ -0,0 +1,45 @@
+﻿using BcTool.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ViewCell), typeof(CustomViewCellRenderer))]
+namespace BcTool.iOS.Renderers
+{
+    /// <summary>
+    /// Custom view cell renderer.
+    /// </summary>
+    public class CustomViewCellRenderer : ViewCellRenderer
+	{
+        /// <summary>
+        /// Gets the cell.
+        /// </summary>
+        /// <returns>The cell.</returns>
+        /// <param name="item">Item.</param>
+        /// <param name="reusableCell">Reusable cell.</param>
+        /// <param name="tv">Tv.</param>
+		public override UITableViewCell GetCell(Cell item, UITableViewCell reusableCell, UITableView tv)
+		{
+			var cell = base.GetCell(item, reusableCell, tv);
+			
+            if (cell != null)
+			{
+				//iOS8/iOS9対応
+				if (!UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+				{
+					foreach (UITableViewCell tvCell in tv.VisibleCells)
+					{
+						tvCell.SeparatorInset = UIEdgeInsets.Zero;
+						if (UIDevice.CurrentDevice.CheckSystemVersion(8, 0))
+						{
+							tvCell.LayoutMargins = UIEdgeInsets.Zero;
+							tvCell.PreservesSuperviewLayoutMargins = false;
+						}
+					}
+				}
+			}
+
+			return cell;
+		}
+	}
+}
