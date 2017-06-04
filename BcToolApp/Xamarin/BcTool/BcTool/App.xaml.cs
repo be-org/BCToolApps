286b342d2e593dd9a4ea95edebc14fc8b5321bc9
Taku Hatanaka <taku-hatanaka@be-org.co.jp>
2017-06-04 13:38:16 +0900

    Xamarin.Forms、BTProgressHUDの更新

--- BcToolApp/Xamarin/BcTool/BcTool/App.xaml.cs
+++ BcToolApp/Xamarin/BcTool/BcTool/App.xaml.cs
@@ -4,32 +4,32 @@ using Xamarin.Forms;
 
 namespace BcTool
 {
-	public partial class App : PrismApplication
-	{
-		public App(IPlatformInitializer initializer = null) : base(initializer) { }
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
 
-		protected override void OnInitialized()
-		{
-			InitializeComponent();
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
 
             NavigationService.NavigateAsync(nameof(SigninPage));
             //NavigationService.NavigateAsync(nameof(MasterPage));
             //NavigationService.NavigateAsync("NavigationPage/" + nameof(BulletinBoardTabbedPage));
         }
 
-		protected override void RegisterTypes()
-		{
-			Container.RegisterTypeForNavigation<NavigationPage>();
-			Container.RegisterTypeForNavigation<SigninPage>();
-			Container.RegisterTypeForNavigation<MenuPage>();
-			Container.RegisterTypeForNavigation<MasterPage>();
-			Container.RegisterTypeForNavigation<SettingPage>();
-			Container.RegisterTypeForNavigation<BulletinBoardPage>();
-			Container.RegisterTypeForNavigation<BulletinBoardEditPage>();
-			Container.RegisterTypeForNavigation<BulletinBoardTabbedPage>();
-			Container.RegisterTypeForNavigation<BulletinBoardInfoPage>();
-			Container.RegisterTypeForNavigation<UserManagePage>();
-			Container.RegisterTypeForNavigation<PasswordChangePage>();
-		}
-	}
+        protected override void RegisterTypes()
+        {
+            Container.RegisterTypeForNavigation<NavigationPage>();
+            Container.RegisterTypeForNavigation<SigninPage>();
+            Container.RegisterTypeForNavigation<MenuPage>();
+            Container.RegisterTypeForNavigation<MasterPage>();
+            Container.RegisterTypeForNavigation<SettingPage>();
+            Container.RegisterTypeForNavigation<BulletinBoardPage>();
+            Container.RegisterTypeForNavigation<BulletinBoardEditPage>();
+            Container.RegisterTypeForNavigation<BulletinBoardTabbedPage>();
+            Container.RegisterTypeForNavigation<BulletinBoardInfoPage>();
+            Container.RegisterTypeForNavigation<UserManagePage>();
+            Container.RegisterTypeForNavigation<PasswordChangePage>();
+        }
+    }
 }
